0857ef08dcb839c0e9527b9d04fe980eb4730926
LARRYSONB <larrysonbiya@gmail.com>
2026-08-20 21:18:06 -0400

    creating carmodel et carmake object

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,17 +6,21 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # path for registration
+    # # path for registration
     path(route='register', view=views.registration, name='register'),
-
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
-    # path for logout
+    # path to logout
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
     # path for add a review view
+
+    # path to get car
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
+    
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
