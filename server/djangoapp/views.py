17b644d525d3b6ff6d1ac1ed3316c2f391227b71
LARRYSONB <larrysonbiya@gmail.com>
2026-08-21 15:25:27 -0400

    created getcar views

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -139,12 +139,15 @@ def add_review(request):
 
 
 def get_cars(request):
-    if CarMake.objects.count() == 0 or CarModel.objects.count() == 0:
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels": cars})
+    return JsonResponse({"CarModels":cars})
+
     
 
