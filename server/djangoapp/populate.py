8f1a75fff0e2a07ebb53ab6c17084faa06a78aaf
LARRYSONB <133322555+LARRYSONB@users.noreply.github.com>
2026-08-28 10:25:53 -0400

    Remove 'type' field from car model data
    Updated car model data to remove 'type' field.

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -15,21 +15,21 @@ def initiate():
 
     # Create CarModel instances including the required dealer_id field
     car_model_data = [
-        {"name": "Pthfder", "type": "SUV", "year": 23, "dealer_id": 1, "car_make": c_m_i[0]},
-        {"name": "Qashqai", "type": "SUV", "year": 23, "dealer_id": 1, "car_make": c_m_i[0]},
-        {"name": "XTRAIL", "type": "SUV", "year": 23, "dealer_id": 1, "car_make": c_m_i[0]},
-        {"name": "A-Class", "type": "SUV", "year": 23, "dealer_id": 2, "car_make": c_m_i[1]},
-        {"name": "C-Class", "type": "SUV", "year": 23, "dealer_id": 2, "car_make": c_m_i[1]},
-        {"name": "E-Class", "type": "SUV", "year": 23, "dealer_id": 2, "car_make": c_m_i[1]},
-        {"name": "A4", "type": "SUV", "year": 23, "dealer_id": 3, "car_make": c_m_i[2]},
-        {"name": "A5", "type": "SUV", "year": 23, "dealer_id": 3, "car_make": c_m_i[2]},
-        {"name": "A6", "type": "SUV", "year": 23, "dealer_id": 3, "car_make": c_m_i[2]},
-        {"name": "Sorrento", "type": "SUV", "year": 23, "dealer_id": 4, "car_make": c_m_i[3]},
-        {"name": "Carnival", "type": "SUV", "year": 23, "dealer_id": 4, "car_make": c_m_i[3]},
-        {"name": "Cerato", "type": "Sedan", "year": 23, "dealer_id": 4, "car_make": c_m_i[3]},
-        {"name": "Corolla", "type": "Sedan", "year": 23, "dealer_id": 5, "car_make": c_m_i[4]},
-        {"name": "Camry", "type": "Sedan", "year": 23, "dealer_id": 5, "car_make": c_m_i[4]},
-        {"name": "Kluger", "type": "SUV", "year": 23, "dealer_id": 5, "car_make": c_m_i[4]},
+        {"name": "Pthfder", "year": 23, "dealer_id": 1, "car_make": c_m_i[0]},
+        {"name": "Qashqai", ""year": 23, "dealer_id": 1, "car_make": c_m_i[0]},
+        {"name": "XTRAIL", "year": 23, "dealer_id": 1, "car_make": c_m_i[0]},
+        {"name": "A-Class", "year": 23, "dealer_id": 2, "car_make": c_m_i[1]},
+        {"name": "C-Class", "year": 23, "dealer_id": 2, "car_make": c_m_i[1]},
+        {"name": "E-Class", "year": 23, "dealer_id": 2, "car_make": c_m_i[1]},
+        {"name": "A4", "year": 23, "dealer_id": 3, "car_make": c_m_i[2]},
+        {"name": "A5", "year": 23, "dealer_id": 3, "car_make": c_m_i[2]},
+        {"name": "A6","year": 23, "dealer_id": 3, "car_make": c_m_i[2]},
+        {"name": "Sorrento", "year": 23, "dealer_id": 4, "car_make": c_m_i[3]},
+        {"name": "Carnival", "year": 23, "dealer_id": 4, "car_make": c_m_i[3]},
+        {"name": "Cerato", "year": 23, "dealer_id": 4, "car_make": c_m_i[3]},
+        {"name": "Corolla", "year": 23, "dealer_id": 5, "car_make": c_m_i[4]},
+        {"name": "Camry", "year": 23, "dealer_id": 5, "car_make": c_m_i[4]},
+        {"name": "Kluger", "year": 23, "dealer_id": 5, "car_make": c_m_i[4]},
     ]
 
     for data in car_model_data:
